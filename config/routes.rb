df160e541d2c49ef086175046c2ed21cc5100c92
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-04-07 01:49:19 +0200

    Add route to BenchmarkDefinitions#index

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ CloudBenchmarking::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'benchmark_definitions#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
