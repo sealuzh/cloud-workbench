1f9f6896e2ce39cf7ca3e987e30f16bc1867d0b9
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-04-07 11:08:27 +0200

    Add BenchmarkExecution status update and fix mixing up execution and definition on save

--- app/jobs/prepare_benchmark_execution_job.rb
+++ app/jobs/prepare_benchmark_execution_job.rb
@@ -4,14 +4,16 @@ class PrepareBenchmarkExecutionJob < Struct.new(:benchmark_definition_id, :bench
     benchmark_definition = BenchmarkDefinition.find(benchmark_definition_id)
     benchmark_execution = BenchmarkExecution.find(benchmark_execution_id)
 
+    benchmark_execution.status = 'PREPARING'
     benchmark_execution.start_time = Time.now
-    benchmark_definition.save
+    benchmark_execution.save
 
     %x( cd "#{benchmark_definition.vagrant_directory_path}";
         vagrant up )
     # TODO: Handle stdout, stderr redirection into a logging directory (not . due to vagrant sync, which may result in an endless loop) and exit_code
 
     benchmark_execution.end_time = Time.now
+    benchmark_execution.status = 'WAITING'
     benchmark_execution.save
   end
 end
