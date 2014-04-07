bcb1a5ca7218b387d60e1fe0cec1bcf13a089311
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-04-08 00:02:29 +0200

    Add StartBenchmarkExecutionJob and start benchmark after its preparation has been finished

---
+++ app/jobs/start_benchmark_execution_job.rb
@@ -0,0 +1,13 @@
+class StartBenchmarkExecutionJob < Struct.new(:benchmark_definition_id, :benchmark_execution_id)
+  def perform
+    benchmark_definition = BenchmarkDefinition.find(benchmark_definition_id)
+    benchmark_execution = BenchmarkExecution.find(benchmark_execution_id)
+
+    %x( cd "#{benchmark_definition.vagrant_directory_path}" &&
+        vagrant ssh -- "sudo bash -c 'cd /usr/local/cloud-benchmark && pwd; nohup ./start > stdout.log 2> stderr.log < /dev/null &'" )
+    # TODO: Handle exit code, Find a way to get the pid
+
+    benchmark_execution.status = 'RUNNING'
+    benchmark_execution.save
+  end
+end
