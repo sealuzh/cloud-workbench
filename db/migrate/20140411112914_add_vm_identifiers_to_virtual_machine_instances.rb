db8eb901a1b807711d1b62c3754507afd8e175fc
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-04-11 18:23:03 +0200

    Add virtual machine identifiers (missing migration required for vm identification)

---
+++ db/migrate/20140411112914_add_vm_identifiers_to_virtual_machine_instances.rb
@@ -0,0 +1,8 @@
+class AddVmIdentifiersToVirtualMachineInstances < ActiveRecord::Migration
+  def change
+    add_column :virtual_machine_instances, :provider_name, :string
+    add_column :virtual_machine_instances, :provider_instance_id, :string
+
+    add_index :virtual_machine_instances, [:provider_instance_id, :provider_name], name: 'index_vm_instances_on_provider_instance_id_and_provider_name'
+  end
+end
