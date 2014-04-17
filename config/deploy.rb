9c2e66b59ba8c379f1cd915046214faf25bd3e25
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-04-17 17:15:40 +0200

    Symlink benchmark_definitions to keep them between deployments and avoid permission issues on cleanup as the rails app does not grant write permission to the apps group

--- config/deploy.rb
+++ config/deploy.rb
@@ -61,12 +61,10 @@ set(:config_files, [
 set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-# TODO: also symlink: public/benchmark_definitions
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/benchmark_definitions}
 
-# Shouldn't be necessary?! => solve problems with Vagrant first!
-# set :default_env, { BUNDLE_GEMFILE: "#{fetch(:deploy_to)}/current/Gemfile",
-#                     BUNDLE_PATH: "#{fetch(:deploy_to)}/shared/vendor/bundle" }
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
