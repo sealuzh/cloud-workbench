7f9e44095859e9a7d8198c8ce799831ea59388e3
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-04-19 17:28:08 +0200

    Add application cookbook for server

---
+++ chef-repo/site-cookbooks/cloud-benchmarking/metadata.rb
@@ -0,0 +1,8 @@
+name             'cloud-benchmarking'
+maintainer       'seal uzh'
+maintainer_email 'joel.scheuner.dev@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures cloud-benchmarking'
+long_description 'Installs/Configures cloud-benchmarking'
+version          '0.1.0'
+
