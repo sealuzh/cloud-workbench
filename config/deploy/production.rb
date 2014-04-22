fd5303b0f4a3fd515624a6133ae2247066895276
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-04-22 04:16:00 +0200

    Add aws to production and move local deployment to staging

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
 set :rails_env, :production # dont try and infer something as important as environment from stage name.
-server '33.33.33.10', user: 'deploy', roles: %w{web app db}
+server '54.73.94.83', user: 'deploy', roles: %w{web app db}
