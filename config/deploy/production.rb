b5815e554f700723b1ea1bad852de14370807671
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-04-22 16:19:37 +0200

    Update ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
 set :rails_env, :production # dont try and infer something as important as environment from stage name.
-server '54.73.94.83', user: 'deploy', roles: %w{web app db}
+server '46.137.2.62', user: 'deploy', roles: %w{web app db}
