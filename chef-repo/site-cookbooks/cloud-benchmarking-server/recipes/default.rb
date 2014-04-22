f9559866e788041dcf3f1afbf43f3dfb3e729cc2
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-04-23 00:58:41 +0200

    Add delayed_job worker runit configuration

--- chef-repo/site-cookbooks/cloud-benchmarking-server/recipes/default.rb
+++ chef-repo/site-cookbooks/cloud-benchmarking-server/recipes/default.rb
@@ -27,12 +27,15 @@ include_recipe "cbench-nodejs"
 # Consider configuring the database.yml based on the chosen database, password and db name here
 # instead of pushing this configuration later via Capistrano.
 
-# TODO: Needs testing! A paremetrized loop may be required for multiple workers!?
+
+
+# TODO: A paremetrized loop may be required for multiple workers!?
 # Refactor into own recipe later
-=begin
 # Delayed job worker(s)
+app = {}
 app["appname"] = "cloud_benchmarking" # TODO: Fix hardcoded value.
 app_dir = ::File.join(node["appbox"]["apps_dir"], app["appname"], 'current')
+home_dir = File.join(node['user']['home_root'], node['appbox']['apps_user']
 runit_service "delayed_job" do
 	run_template_name  node["cloud-benchmarking-server"]["delayed_job"]["template_name"]
 	log_template_name  node["cloud-benchmarking-server"]["delayed_job"]["template_name"]
@@ -42,16 +45,20 @@ runit_service "delayed_job" do
 		:group                => node["appbox"]["apps_user"],
 		:rack_env             => node["cloud-benchmarking-server"]["delayed_job"]["env"],
     :working_directory    => app_dir,
-    :app_name             => app["appname"]
+    :service_name         => "delayed_job",
+    :home_dir             => home_dir
   )
+
+  # TODO: provide as configurable attribute instead of hardcoding here
   # The BUNDLE_GEMFILE environment variable is required if running rails apps with unicorn.
   # Otherwise unicorn would fail on startup with a 'Bundler::GemfileNotFound' exception searching
   # within another directory for the Gemfile (e.g. searching in 'shared/Gemfile')
   # See: http://blog.willj.net/2011/08/02/fixing-the-gemfile-not-found-bundlergemfilenotfound-error/
   env(
     'BUNDLE_GEMFILE'      => File.join(app_dir, 'Gemfile'),
-    'BUNDLE_PATH'         => File.absolute_path(File.join(app_dir, '../shared/vendor/bundle')) # Symlinked to shared/vendor/bundle
+    'BUNDLE_PATH'         => File.absolute_path(File.join(app_dir, '../shared/vendor/bundle')), # Symlinked to shared/vendor/bundle
+    'RAILS_ENV'           => 'production',
+    'HOME'                => home_dir)
   )
 	restart_on_update false
 end
-=end
