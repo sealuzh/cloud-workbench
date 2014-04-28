25f505470a040c63fe27fc807e58e4da39f2b262
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-04-28 23:34:10 +0200

    Update database.yml example

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,2 +1,2 @@
-set :rails_env, :staging # dont try and infer something as important as environment from stage name.
+set :rails_env, :production # dont try and infer something as important as environment from stage name.
 server '54.228.71.158', user: 'deploy', roles: %w{web app db}
