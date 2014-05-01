1b16e5b8664140211738c2f31ef52db84f1a093c
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-05-01 21:48:06 +0200

    Handle cron2english parse exception

--- app/models/benchmark_schedule.rb
+++ app/models/benchmark_schedule.rb
@@ -17,8 +17,13 @@ class BenchmarkSchedule < ActiveRecord::Base
   before_destroy :update_system_crontab_if_active
   after_update :check_and_update_system_crontab_after_update
 
+  # The best known to english translator has been found here:
+  # http://www.joostbrugman.com/bitsofthought/page/4/Converting_Crontab_Entries_To_Plain_English_%28Or_Any_Other_Language%29
+
   def cron_expression_in_english
     Cron2English.parse(self.cron_expression).join(' ')
+  rescue Cron2English::ParseException => e
+    e.message
   end
 
   def self.actives
