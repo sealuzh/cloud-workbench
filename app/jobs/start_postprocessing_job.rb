4088f682298b4d912b803fedb56239be37e9417d
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-05-05 19:43:08 +0200

    Fix undefined variable within start postprocessing job

--- app/jobs/start_postprocessing_job.rb
+++ app/jobs/start_postprocessing_job.rb
@@ -1,6 +1,6 @@
 class StartPostprocessingJob < Struct.new(:benchmark_execution_id)
   def perform
-    benchmark_execution = BenchmarkExecution.find(benchmark_definition_id)
+    benchmark_execution = BenchmarkExecution.find(benchmark_execution_id)
     benchmark_execution.start_postprocessing
   end
 end
