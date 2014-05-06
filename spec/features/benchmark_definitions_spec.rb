9126b81ebf68c8b4a2c3c617720a32132af21672
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-05-07 01:06:04 +0200

    Refactor benchmark definition acceptance tests

--- spec/features/benchmark_definitions_spec.rb
+++ spec/features/benchmark_definitions_spec.rb
@@ -7,41 +7,46 @@ feature "Benchmark definition management" do
     page.should have_link('Create New Benchmark', href: new_benchmark_definition_path)
   end
 
-  scenario "Creating a valid benchmark definition" do
-    valid_benchmark_definition = build(:benchmark_definition)
-    visit new_benchmark_definition_path
-    expect(find_field('Vagrantfile').value).to match(/.*Vagrant.configure.*/)
-    within("#new_benchmark_definition") do
-      fill_in 'Name', with: valid_benchmark_definition.name
-      fill_in 'Vagrantfile', with: valid_benchmark_definition.vagrantfile
+  feature "Creating a benchmark definition" do
+    background { visit new_benchmark_definition_path }
+
+    scenario "Should provide a sample Vagrantfile" do
+      expect(find_field('Vagrantfile').value).to match(/.*Vagrant.configure.*/)
     end
 
-    click_button 'Create New Benchmark'
-    page.should have_content 'Benchmark definition was successfully created'
-    # TODO: decide about show and edit
-    # page.should have_content valid_benchmark_definition.name
-    page.should have_field('Name', with: valid_benchmark_definition.name)
-    # page.should have_content valid_benchmark_definition.vagrantfile
-    page.should have_field('Vagrantfile', with: valid_benchmark_definition.vagrantfile)
-    bm_definition = BenchmarkDefinition.find_by_name(valid_benchmark_definition.name)
-    bm_definition.vagrantfile .should eq valid_benchmark_definition.vagrantfile
+    scenario "With valid information" do
+      valid_benchmark_definition = build(:benchmark_definition)
+      fill_in_create_form(valid_benchmark_definition)
+
+      click_button 'Create New Benchmark'
+      page.should have_content 'Benchmark definition was successfully created'
+      page.should have_field('Name', with: valid_benchmark_definition.name)
+      page.should have_field('Vagrantfile', with: valid_benchmark_definition.vagrantfile)
+      bm_definition = BenchmarkDefinition.find_by_name(valid_benchmark_definition.name)
+      bm_definition.vagrantfile .should eq valid_benchmark_definition.vagrantfile
+    end
+
+    given(:existing_definition) { create(:benchmark_definition) }
+    scenario "With invalid information" do
+      same_name_definition = build(:benchmark_definition, name: existing_definition.name)
+      visit new_benchmark_definition_path
+      fill_in_create_form(same_name_definition)
+
+      expect do
+        click_button 'Create New Benchmark'
+      end.to change(BenchmarkDefinition, :count).by(0)
+      page.should have_content 'has already been taken'
+    end
+
+    def fill_in_create_form(benchmark_definition)
+      within("#new_benchmark_definition") do
+        fill_in 'Name', with: benchmark_definition.name
+        fill_in 'Vagrantfile', with: benchmark_definition.vagrantfile
+      end
+    end
   end
 
   given(:existing_definition) { create(:benchmark_definition) }
-  scenario "Creating an invalid benchmark definition" do
-    same_name_definition = build(:benchmark_definition, name: existing_definition.name)
-    visit new_benchmark_definition_path
-    within("#new_benchmark_definition") do
-      fill_in 'Name', with: same_name_definition.name
-      fill_in 'Vagrantfile', with: same_name_definition.vagrantfile
-    end
-
-    expect do
-      click_button 'Create New Benchmark'
-    end.to change(BenchmarkDefinition, :count).by(0)
-    page.should have_content 'has already been taken'
-  end
-
   scenario "Show a benchmark definition" do
     visit benchmark_definition_path(existing_definition)
     page.should have_content existing_definition.name
@@ -49,39 +54,48 @@ feature "Benchmark definition management" do
     pending("should list vm instances")
   end
 
-  context "Editing a benchmark definition" do
+  feature "Editing a benchmark definition" do
     given(:benchmark_definition) { create(:benchmark_definition) }
     background { visit edit_benchmark_definition_path(benchmark_definition) }
-    given(:start_execution) { -> { click_button 'Start Execution'} }
 
-    scenario "Name should be readonly" do
-      page.should_not have_xpath("//input[@id='benchmark_definition_name' and @readonly='readonly']")
+    context "Without existing benchmark executions" do
+      scenario "Name field should be editable" do
+        page.should_not have_xpath("//input[@id='benchmark_definition_name' and @readonly='readonly']")
+      end
     end
 
-    scenario "Start an execution should create a new benchmark execution" do
-      expect(start_execution).to change(BenchmarkExecution, :count).by(1)
+    context "With existing benchmark executions" do
+      background do
+        benchmark_definition.start_execution_async
+        visit edit_benchmark_definition_path(benchmark_definition)
+      end
+
+      scenario "Name field should be readonly" do
+        page.should have_xpath("//input[@id='benchmark_definition_name' and @readonly='readonly']")
+      end
     end
 
-    scenario "Start an execution should schedule a job" do
-      expect(start_execution).to change(Delayed::Job, :count).by(1)
-    end
-
-    scenario "Start an execution should redirect to the newly created benchmark execution " do
-      start_execution.call
-      execution = BenchmarkExecution.find_by_benchmark_definition_id(benchmark_definition.id)
-      expect(current_path).to eq benchmark_execution_path(execution)
-    end
   end
 
-  context "Existing benchmark executions" do
+  feature "Starting a benchmark execution" do
     given(:benchmark_definition) { create(:benchmark_definition) }
-    background do
-      benchmark_definition.start_execution_async
-      visit edit_benchmark_definition_path(benchmark_definition)
-    end
-    feature "Edit a benchmark definition" do
-      scenario "Name should be readonly" do
-        page.should have_xpath("//input[@id='benchmark_definition_name' and @readonly='readonly']")
+
+    context "Within editing a benchmark definition" do
+      background { visit edit_benchmark_definition_path(benchmark_definition) }
+      given(:start_execution) { -> { click_button 'Start Execution'} }
+
+      scenario "Should create a new benchmark execution" do
+        expect(start_execution).to change(BenchmarkExecution, :count).by(1)
+      end
+
+      scenario "Should schedule a job" do
+        expect(start_execution).to change(Delayed::Job, :count).by(1)
+      end
+
+      scenario "Should redirect to the newly created benchmark execution " do
+        start_execution.call
+        execution = BenchmarkExecution.find_by_benchmark_definition_id(benchmark_definition.id)
+        expect(current_path).to eq benchmark_execution_path(execution)
       end
     end
   end
