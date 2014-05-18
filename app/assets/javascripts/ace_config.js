67719ff2fd7805e70c3429c1e9f59efbdf0436df
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-05-18 17:13:18 +0200

    Improve benchmark_definition#show and common edit form

--- app/assets/javascripts/ace_config.js
+++ app/assets/javascripts/ace_config.js
@@ -6,29 +6,33 @@
 
 var tabSize = 2;
 var wrapLimit = null;
+var minHeight = 400;
 // Make sure you import the theme and mode to use in application.js
 var ace_theme = 'github';
 var ace_mode = 'ruby';
 var configure_ace_editor = function() {
     var textarea = $('#benchmark_definition_vagrantfile');
     if (textarea.exists()) {
+        // Replace textarea with ace editor
         textarea.acedInitTA({ theme: ace_theme,
                               mode: ace_mode
         });
+
+        // Dynamically resize the editor height
         var ace_div = $('.ace_editor');
         var set_height = function() {
-            ace_div.css({ height: $(window).innerHeight() * 0.7 });
+            var height = Math.max(minHeight, $(window).innerHeight() * 0.7);
+            ace_div.css({ height: height });
         };
-
         set_height();
         $(window).resize(function(){
             set_height();
         });
 
+        // Configure editor
         var editor = textarea.data('ace-div').aced();
         var session = editor.getSession();
         session.setTabSize(tabSize);
-
         // Example how to set theme and mode via javascript
         //    editor.setTheme("ace/theme/monokai");
         //    session.setMode("ace/mode/ruby");
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  def content_header(title, subtitle = '')
+    content_for :content_header do
+      "<h1>#{title}<small>#{subtitle}</small></h1>".html_safe
+    end
+  end
+
   def formatted_time(time)
     time.present? ? time.to_formatted_s(:db) : '-'
   end
