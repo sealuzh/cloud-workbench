431f8b28db2842b62a9f18098cc7bb98bbcb289a
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-05-18 02:44:57 +0200

    Minor refactoring and fix erb template names

--- app/helpers/benchmark_schedules_helper.rb
+++ app/helpers/benchmark_schedules_helper.rb
@@ -6,4 +6,18 @@ module BenchmarkSchedulesHelper
       link_to "#{start_icon}&nbsp;Activate".html_safe, activate_benchmark_schedule_path(schedule), method: :patch, class: 'activate-color'
     end
   end
+
+  def benchmark_schedule_label(schedule)
+    if schedule.active?
+      schedule_label(schedule, 'success')
+    else
+      schedule_label(schedule, 'default')
+    end
+  end
+
+  private
+
+    def schedule_label(schedule, type)
+      link_to schedule.cron_expression, schedule, class: "label label-#{type} schedule-align", title: schedule.cron_expression_in_english.html_safe
+    end
 end
