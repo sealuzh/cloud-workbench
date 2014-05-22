6db8279470502866c83fb496f2b48bec6cc9a160
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-05-22 04:47:25 +0200

    Update staging deploy.rb

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,4 @@
 set :rails_env, :production # dont try and infer something as important as environment from stage name.
-server '54.228.71.158', user: 'deploy', roles: %w{web app db}
-# set :delayed_job_workers, 1
+server '54.195.193.94', user: 'deploy', roles: %w{web app db}
+set :delayed_job_workers, 2
+set :branch, 'admin-lte-theme'
