bf39e2ef0a4738b0178e4c5ca553edc832bd33e8
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-05-26 18:32:57 +0200

    Show 24h time on lockscreen

--- app/assets/javascripts/sign_in.js
+++ app/assets/javascripts/sign_in.js
@@ -1,12 +1,3 @@
-$(function() {
-    startTime();
-    $(".center").center();
-    $(window).resize(function() {
-        $(".center").center();
-    });
-});
-
-/*  */
 function startTime()
 {
     var today = new Date();
@@ -18,15 +9,8 @@ function startTime()
     m = checkTime(m);
     s = checkTime(s);
 
-    //Check for PM and AM
-    var day_or_night = (h > 11) ? "PM" : "AM";
-
-    //Convert to 12 hours system
-    if (h > 12)
-        h -= 12;
-
     //Add time to the headline and update every 500 milliseconds
-    $('#time').html(h + ":" + m + ":" + s + " " + day_or_night);
+    $('#time').html(h + ":" + m + ":" + s + " ");
     setTimeout(function() {
         startTime()
     }, 500);
@@ -42,11 +26,19 @@ function checkTime(i)
 }
 
 /* CENTER ELEMENTS IN THE SCREEN */
-jQuery.fn.center = function() {
+jQuery.fn.center_sign_in = function() {
     this.css("position", "absolute");
     this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) +
-        $(window).scrollTop()) - 30 + "px");
+        $(window).scrollTop()) - 60 + "px");
     this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) +
         $(window).scrollLeft()) + "px");
     return this;
 }
+
+$(document).on('ready page:load', function () {
+    startTime();
+    $(".center").center_sign_in();
+    $(window).resize(function() {
+        $(".center").center_sign_in();
+    });
+});
