1c0d09fea96fbc7523917245f9f3f6615d082230
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-05-27 03:39:04 +0200

    Fix uncommented email index for devise user model

--- db/migrate/20140524232230_devise_create_users.rb
+++ db/migrate/20140524232230_devise_create_users.rb
@@ -34,7 +34,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    # add_index :users, :email,                unique: true
+    add_index :users, :email,                unique: true
     # add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
