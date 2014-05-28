a3a4e304aaf056a66663ecf3d8e6d5ca2a975993
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-05-28 13:37:33 +0200

    Update deployment ips and add old staging instance

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 set :rails_env, :production # dont try and infer something as important as environment from stage name.
-server '54.220.84.9', user: 'deploy', roles: %w{web app db}
-# set :delayed_job_workers, 1
+server '54.246.33.241', user: 'deploy', roles: %w{web app db}
+set :delayed_job_workers, 4
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 set :rails_env, :production # dont try and infer something as important as environment from stage name.
-server '176.34.93.33', user: 'deploy', roles: %w{web app db}
+server '54.73.29.244', user: 'deploy', roles: %w{web app db}
 set :delayed_job_workers, 2
 # set :branch, 'stabilization-testing'
