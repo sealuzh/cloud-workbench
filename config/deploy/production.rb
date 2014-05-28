abd9127cd600ac3ae06ce217735c898d21599833
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-05-28 15:56:44 +0200

    Rotate environements: production => retired, staging => production, staging_OLD => staging

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 set :rails_env, :production # dont try and infer something as important as environment from stage name.
-server '54.246.33.241', user: 'deploy', roles: %w{web app db}
-set :delayed_job_workers, 4
+server '54.73.29.244', user: 'deploy', roles: %w{web app db}
+set :delayed_job_workers, 8
--- config/deploy/retired.rb
+++ config/deploy/retired.rb
@@ -1,4 +1,4 @@
 set :rails_env, :production # dont try and infer something as important as environment from stage name.
-server '33.33.33.10', user: 'deploy', roles: %w{web app db}
-set :delayed_job_workers, 2
+server '54.246.33.241', user: 'deploy', roles: %w{web app db}
+set :delayed_job_workers, 4
 # set :branch, 'branch-name'
