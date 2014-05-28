ef6c6b64168e2965eb25e17a845c08450757d1ca
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-05-28 11:29:39 +0200

    Improve the provided Vagrantfile example with basic documentation; Reset deployment branches to master

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 set :rails_env, :production # dont try and infer something as important as environment from stage name.
 server '176.34.93.33', user: 'deploy', roles: %w{web app db}
 set :delayed_job_workers, 2
-set :branch, 'stabilization-testing'
+# set :branch, 'stabilization-testing'
