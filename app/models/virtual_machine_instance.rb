3351e03cd8302a500480871634f9161147532122
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-06-24 21:04:07 +0200

    Fix missing dependent destroy behavior that prevented metric observations from deletion

--- app/models/virtual_machine_instance.rb
+++ app/models/virtual_machine_instance.rb
@@ -1,8 +1,8 @@
 class VirtualMachineInstance < ActiveRecord::Base
   belongs_to :benchmark_execution
   validates :benchmark_execution, presence: true
-  has_many :nominal_metric_observations
-  has_many :ordered_metric_observations
+  has_many :nominal_metric_observations, dependent: :destroy
+  has_many :ordered_metric_observations, dependent: :destroy
 
   def complete_benchmark(continue, success = true)
     if success
