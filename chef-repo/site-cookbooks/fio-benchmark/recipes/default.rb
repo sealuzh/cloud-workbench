e754e8534293db65ce35dfc2d27bf40fbde92cc4
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-06-24 20:54:34 +0200

    Update FIO benchmark with refill_buffer and generic cli options

--- chef-repo/site-cookbooks/fio-benchmark/recipes/default.rb
+++ chef-repo/site-cookbooks/fio-benchmark/recipes/default.rb
@@ -17,6 +17,7 @@ template "#{node['benchmark']['dir']}/#{node['benchmark']['start']}" do
   variables(
     ruby: RUBY,
     config_file: node['fio']['config_file'],
+    cli_options: node['fio']['cli_options'],
     fio_log: "#{node['fio']['config']['write_bw_log']}_bw.log",
     metric_definition_id: node['fio']['metric_definition_id'],
     cpu: node['cpu']['0']['model_name']
