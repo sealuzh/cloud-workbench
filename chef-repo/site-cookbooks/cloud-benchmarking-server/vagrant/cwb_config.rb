6f2070b29f55eb5576259a7e2c4e1b44670337dd
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-07-17 15:52:58 +0200

    Update Vagrantfiles for CWB Server

--- chef-repo/site-cookbooks/cloud-benchmarking-server/vagrant/cwb_config.rb
+++ chef-repo/site-cookbooks/cloud-benchmarking-server/vagrant/cwb_config.rb
@@ -22,6 +22,8 @@ class CwbConfig
       if @apply_secrets
         path = "#{@config_dir}/#{name}"
         File.read(path)
+      else
+        SECRETS_NOT_APPLIED
       end
     rescue => e
       puts "WARNING: Config file at #{path} does not exist. #{e.message}"
@@ -59,12 +61,10 @@ class CwbConfig
     end
 
     def load_yaml_config(name)
-      if @apply_secrets
-        path = "#{@config_dir}/#{name}"
-        YAML.load_file(path)
-      end
+      path = "#{@config_dir}/#{name}"
+      YAML.load_file(path)
     rescue => e
-      puts "WARINING: No config file found in #{config_file}. #{e.message}"
+      puts "WARINING: No config file found in #{path}. #{e.message}"
       'CONFIG FILE NOT FOUND'
     end
 
@@ -74,15 +74,15 @@ class CwbConfig
           'cloud-benchmarking-server' => {
               'apply_secret_config' => @apply_secrets,
               'chef' => {
-                  'server_ip'       => string_from_config(config, 'chef', 'server_ip'),
+                  'server_ip'       => config['chef']['server_ip'],
                   'node_name'       => 'cloud-benchmarking',
                   'client_key_name' => 'cloud-benchmarking',
-                  'client_key'      => string_from_file('chef_client_key.pem') || 'EMPTY',
-                  'validator_key'   => string_from_file('chef_validator.pem')  || 'EMPTY',
+                  'client_key'      => string_from_file('chef_client_key.pem'),
+                  'validator_key'   => string_from_file('chef_validator.pem'),
               },
               'aws' => {
                   'ssh_key_name' => 'cloud-benchmarking',
-                  'ssh_key'      => string_from_file('cloud-benchmarking.pem') || '',
+                  'ssh_key'      => string_from_file('cloud-benchmarking.pem'),
                   'access_key'   => string_from_config(config, 'aws', 'access_key'),
                   'secret_key'   => string_from_config(config, 'aws', 'secret_key'),
               },
@@ -98,14 +98,9 @@ class CwbConfig
           # For a list of time zones see: http://en.wikipedia.org/wiki/List_of_tz_database_time_zones
           'tz' => 'Europe/Zurich',
           'appbox' => {
-              'deploy_keys' => [
-                  'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCaea+LWlsLHA+NaIHDAETymdFToH9FcTvxDGHydE8bxAvlebXq625wF9/YX8qVN6ahcFME32JbNfFSYlp8KGDCa+qNazVawdhsOrEqsudOgZRuizlTI8AjEbbNvVyanQ+c5F/+zLW0v+/N+gk203k7v9lptYlmUQEmLg5EDEwpSrVAVmyfwZr5sMtSa6Ll3WFydGwTxmtGSfzTehMcRHCN/gyk5EOcJScP0PHq6RNSwN6ZPClXuOWBL+2wJ62yNUDm5fPM/X8RgB2IBczO0h7+j51zT85HiE99o5ILfMQjZ/yff6t+qJwJS+DXVZrGs2X3CM3pSxcONKYr9AlTfn+P joel-key-pair-ireland',
-                  'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQCD+BQ5ILiCrn4IIoabtQOz7iAhQV48LfqdSlrxTMIoDrAzKZdyfGlhaRydLz7acdfd/PJG8IzaskinGdQYM3NfkPFniyU72ir07/QykQDbgadEA5XP4o1Tm0hUPs1Wt7OHBWzYxYjhYJN7Rnun7Pc/Xj4D7Dr48FJQUxUvAPLYJQ== joel.scheuner.dev@gmail.com',
-                  'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCmGFGNrQv0JXubuWTb5gXfTUko1KY0aDAERN328aJjjXmckjYkSzufzD532VWXOWinIdZn/Tudsx4XQCWZKVzdatvk6iH7RfSdn7pkdOVmjfuGYFR0FdIieqqfRzEAoxiQH9AqtEXREzcDosTFWJkMwytWjCi6irNCIv+NoedgWyuFYAsgxb5N+o/amei6Rch19tsjTTbIiRcjTyr7/MudT5c5X6FVDoRCI2/0B0DxORDw18IMCgWEtsD0zBAEhHOKhIB+siYH0edV6lFf7qnSOTzQWds9I7aiu2acAJve8VxegFbNbv1gjUTZ01HSSR4dTZUOOwyKT6SwjUxnT9u3 philipp@e215-111.eduroam.tuwien.ac.at'
-              ],
-              'admin_keys' => [
-                  'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCaea+LWlsLHA+NaIHDAETymdFToH9FcTvxDGHydE8bxAvlebXq625wF9/YX8qVN6ahcFME32JbNfFSYlp8KGDCa+qNazVawdhsOrEqsudOgZRuizlTI8AjEbbNvVyanQ+c5F/+zLW0v+/N+gk203k7v9lptYlmUQEmLg5EDEwpSrVAVmyfwZr5sMtSa6Ll3WFydGwTxmtGSfzTehMcRHCN/gyk5EOcJScP0PHq6RNSwN6ZPClXuOWBL+2wJ62yNUDm5fPM/X8RgB2IBczO0h7+j51zT85HiE99o5ILfMQjZ/yff6t+qJwJS+DXVZrGs2X3CM3pSxcONKYr9AlTfn+P joel-key-pair-ireland',
-              ]
+              'deploy_keys' => config['cwb_server']['deploy_keys'],
+              # Use first deploy key as admin key
+              'admin_keys' => config['cwb_server']['deploy_keys'][1]
           },
           # TODO: Hide this detail in cwb-server cookbook
           'cbench-rackbox' => {
@@ -127,11 +122,11 @@ class CwbConfig
               }
           },
           'databox' => {
-              'db_root_password' => 'cloud-ba-Ri-Uv',
+              'db_root_password' => config['cwb_server']['db_root_password'],
               'databases' => {
                   'postgresql' => [
                       { 'username' => 'cloud',
-                        'password' => 'uc-Au',
+                        'password' => config['cwb_server']['db_password'],
                         'database_name' => 'cloud_benchmarking_production' }
                   ]
               }
