ab549cc472c49be9ff83c65b47f36848af574093
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-11-25 17:08:47 +0100

    Fix cloud-benchmarking-server installtion dependency order

--- chef-repo/site-cookbooks/cloud-benchmarking-server/berks-cookbooks/cloud-benchmarking-server/recipes/install.rb
+++ chef-repo/site-cookbooks/cloud-benchmarking-server/berks-cookbooks/cloud-benchmarking-server/recipes/install.rb
@@ -2,10 +2,10 @@ include_recipe 'vim'
 include_recipe 'apt'
 include_recipe 'build-essential'
 include_recipe 'vagrant'
-include_recipe 'cloud-benchmarking-server::install_vagrant_google'
 include_recipe 'cbench-nodejs'
 include_recipe 'cbench-databox'
 include_recipe 'cbench-rackbox'
+include_recipe 'cloud-benchmarking-server::install_vagrant_google'
 
 # Install file permission management utility acl used by Capistrano plugin during deployment
 package 'acl'
