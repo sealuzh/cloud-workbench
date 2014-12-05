c80636d64f164a45204dfb597db814fa3b6deeb6
Joel Scheuner <joel.scheuner.dev@gmail.com>
2014-12-06 00:59:29 +0100

    Remove cbench-nodejs wrapper cookbook because this temporary fix is not required anymore

--- chef-repo/site-cookbooks/cbench-nodejs/CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# 0.1.0
-
-Initial release of cbench-nodejs (wrapper cookbook for nodejs)
-
-* Fixes the issue that nodejs gets rebuild from source on every Chef client run.
--- chef-repo/site-cookbooks/cbench-nodejs/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'cbench-nodejs'
-maintainer       'seal uzh'
-maintainer_email 'joel.scheuner.dev@gmail.com'
-license          'Apache 2.0'
-description      'Wrapper cookbook for nodejs'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-
-depends 'nodejs'
