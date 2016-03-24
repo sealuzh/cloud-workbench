831f5f4077e9750cdec719f5c608563aeba66c4c
Joel Scheuner <joel.scheuner.dev@gmail.com>
2016-03-24 15:04:12 +0100

    Update test environment config
    Change deprecated `config.serve_static_assets` to `config.serve_static_files`

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -12,8 +12,8 @@ CloudBenchmarking::Application.configure do
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
-  # Configure static asset server for tests with Cache-Control for performance.
-  config.serve_static_assets  = true
+  # Configure serving files from the public folder with Cache-Control for performance.
+  config.serve_static_files  = true
   config.static_cache_control = "public, max-age=3600"
 
   # Show full error reports and disable caching.
