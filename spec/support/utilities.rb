483ca35f122bb6fcd909e4308f959393a8ffef59
Joel Scheuner <joel.scheuner.dev@gmail.com>
2016-03-24 17:09:10 +0100

    Convert specs to RSpec 3.4.4 syntax with Transpec
    This conversion is done by Transpec 3.2.1 with the following command:
        transpec
    
    * 1 conversion
        from: failure_message_for_should { }
          to: failure_message { }
    
    * 1 conversion
        from: failure_message_for_should_not { }
          to: failure_message_when_negated { }
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -2,10 +2,10 @@ RSpec::Matchers.define :contain_event do |expected|
   match do |events|
     events.select { |event| event.name.to_s == expected.to_s }.any?
   end
-  failure_message_for_should do |events|
+  failure_message do |events|
     "expected that #{event_names(events)} would contain an event with the name #{expected}"
   end
-  failure_message_for_should_not do |events|
+  failure_message_when_negated do |events|
     "expected that #{event_names(events)} would not contain an event with the name #{expected}"
   end
 
