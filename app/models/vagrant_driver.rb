602952f1215c5c58370abd23d96c4bee5575ffb9
Joel Scheuner <joel.scheuner.dev@gmail.com>
2017-07-13 23:32:50 +0200

    Fix environment variable that causes vagrant failure
    In production environment, the `RUBYLIB` variable caused Vagrant to fail:
    ```
    RUBYLIB=/usr/local/ruby-2.4.1/lib/ruby/gems/2.4.0/gems/bundler-1.15.1/lib
    ```
    
    Error message on `vagrant up`
    ```
    Vagrant experienced a version conflict with some installed plugins!
    This usually happens if you recently upgraded Vagrant. As part of the
    upgrade process, some existing plugins are no longer compatible with
    this version of Vagrant. The recommended way to fix this is to remove
    your existing plugins and reinstall them one-by-one. To remove all
    plugins:
    
        rm -r ~/.vagrant.d/plugins.json ~/.vagrant.d/gems
    
    Note if you have an alternate VAGRANT_HOME environmental variable
    set, the folders above will be in that directory rather than your
    user's home directory.
    
    The error message is shown below:
    
    Bundler could not find compatible versions for gem "bundler":
      In Gemfile:
        vagrant (= 1.8.1) was resolved to 1.8.1, which depends on
          bundler (<= 1.10.6, >= 1.5.2)
    
      Current Bundler version:
        bundler (1.15.1)
    This Gemfile requires a different version of Bundler.
    Perhaps you need to update Bundler by running `gem install bundler`?
    
    Could not find gem 'bundler (<= 1.10.6, >= 1.5.2)', which is required by gem 'vagrant (= 1.8.1)', in any of the sources.
    ```

--- app/models/vagrant_driver.rb
+++ app/models/vagrant_driver.rb
@@ -91,10 +91,18 @@ class VagrantDriver
     def shell(cmd, opts)
       full_cmd = ''
       full_cmd << "cd #{opts[:dir]} && " if opts[:dir]
+      full_cmd << reset_env
       full_cmd << cmd
       full_cmd << shell_log(opts[:log]) if opts[:log]
       system(full_cmd)
       $?.success?
+    end
+
+    # Within the production environment, it could be the case the environment variables pollute
+    # the process and cause vagrant commands to fail
+    # Other variables are: `GEM_HOME='' BUNDLE_GEMFILE='' RUBYLIB='' BUNDLER_VERSION='' BUNDLE_BIN_PATH=''`
+    def reset_env
+      "RUBYLIB='' "
     end
 
     def shell_log(file)
