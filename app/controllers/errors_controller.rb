6db3cec0eba07868c350d2a01fcc6be397f3f153
Joel Scheuner <joel.scheuner.dev@gmail.com>
2018-08-15 16:17:00 +0200

    Fix dynamic error rendering for production
    * Explicitly specify to render html template

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,7 +2,7 @@ class ErrorsController < ApplicationController
   skip_before_action :authenticate_user!, raise: false
 
   def show
-    render status: status, template: "errors/#{status_code}"
+    render status: status, template: "errors/#{status_code}", formats: [:html]
   end
 
   protected
