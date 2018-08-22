5230cf07d014227161817b3df55410d766eed3fa
Joel Scheuner <joel.scheuner.dev@gmail.com>
2018-08-22 21:25:59 +0200

    Fix benchmark schedule updater hook (Rails 5.1 regression)
    Source: https://www.levups.com/en/blog/2017/undocumented-dirty-attributes-activerecord-changes-rails51.html
    
    Using the <attribute>_changed? helper along saved_change_to_<attribute> is now more comprehensible:
    ```ruby
    user = User.last
    user.name                   # => "Bob"
    user.name_changed?          # => false
    user.saved_change_to_name?  # => false
    
    user.name = "Clément"       # => "Clément"
    user.name_changed?          # => true
    user.saved_change_to_name?  # => false
    
    user.save                   # => true
    
    user.name                   # => "Clément"
    user.name_changed?          # => false
    user.saved_change_to_name?  # => true
    ```

---
+++ spec/models/benchmark_schedule_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe BenchmarkSchedule do
+  let(:schedule_file) { Rails.application.config.benchmark_schedule }
+  context 'when schedule is inactive' do
+    let(:schedule) { create(:benchmark_schedule, active: false) }
+    before { File.delete(schedule_file) }
+    describe 'activate schedule' do
+      before do
+        schedule.active = true
+        schedule.save
+      end
+      it 'should add the cron expression to the schedule file' do
+        expect(File.exist?(schedule_file)).to be true
+        expect(File.read(schedule_file)).to include(schedule.cron_expression)
+      end
+    end
+  end
+
+  context 'when schedule is active' do
+    let(:schedule) { create(:benchmark_schedule, active: true) }
+    before { BenchmarkSchedule.update_system_crontab }
+    describe 'inactivate schedule' do
+      it 'should remove the cron expression from the schedule file' do
+        expect(File.read(schedule_file)).not_to include(schedule.cron_expression)
+      end
+    end
+  end
+end
