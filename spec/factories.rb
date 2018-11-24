5a3f15ef92261239c3a587b2b8f87fae0f05d66b
Joel Scheuner <joel.scheuner.dev@gmail.com>
2018-11-24 21:21:52 +0100

    Fix FactoryBot 5 deprecations
    Use dynamic instead of deprecated static attributes

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,9 +3,9 @@ require 'ostruct'
 
 FactoryBot.define do
   factory :benchmark_definition do
-    provider_name 'aws'
+    provider_name { 'aws' }
     sequence(:name) { |n| "fio-benchmark #{n}" }
-    running_timeout 10
+    running_timeout { 10 }
     sequence(:vagrantfile) do |n|
         namespace = OpenStruct.new(n: n)
         file_dir = File.expand_path File.dirname(__FILE__)
@@ -17,8 +17,8 @@ FactoryBot.define do
 
   factory :benchmark_schedule do
     association :benchmark_definition, factory: :benchmark_definition
-    cron_expression '30 * * * *'
-    active true
+    cron_expression { '30 * * * *' }
+    active { true }
   end
 
   factory :benchmark_execution do
@@ -34,9 +34,9 @@ FactoryBot.define do
 
   factory :virtual_machine_instance do
     association :benchmark_execution, factory: :benchmark_execution
-    role 'default'
-    provider_name 'aws'
-    provider_instance_id 'i-0a1daccdf9784de6a'
+    role { 'default' }
+    provider_name { 'aws' }
+    provider_instance_id { 'i-0a1daccdf9784de6a' }
   end
 
   factory :vagrant_file_system do
@@ -48,8 +48,8 @@ FactoryBot.define do
   end
 
   factory :vagrant_driver do
-    vagrantfile_path "#{Rails.application.config.spec_files}/vagrant_driver/single_aws_instance/Vagrantfile"
-    log_dir nil
+    vagrantfile_path { "#{Rails.application.config.spec_files}/vagrant_driver/single_aws_instance/Vagrantfile" }
+    log_dir { nil }
 
     skip_create
     initialize_with { new(vagrantfile_path, log_dir) }
@@ -57,18 +57,18 @@ FactoryBot.define do
 
   factory :metric_definition do
     association :benchmark_definition, factory: :benchmark_definition
-    name 'default metric'
-    scale_type 'ratio'
+    name { 'default metric' }
+    scale_type { 'ratio' }
 
     factory :ratio_metric_definition do
-      name 'seq. write'
-      unit 'mb/s'
-      scale_type 'ratio'
+      name { 'seq. write' }
+      unit { 'mb/s' }
+      scale_type { 'ratio' }
     end
 
     factory :nominal_metric_definition do
-      name 'cpu type'
-      scale_type 'nominal'
+      name { 'cpu type' }
+      scale_type { 'nominal' }
     end
   end
 
@@ -76,18 +76,18 @@ FactoryBot.define do
     association :virtual_machine_instance, factory: :virtual_machine_instance
     # NOTE: metric might be associated with other benchmark_definition than created via virtual_machine_instance dependent factories
     association :metric_definition, factory: :ratio_metric_definition #, benchmark_definition: virtual_machine_instance.benchmark_execution.benchmark_definition
-    value 5.1
+    value { 5.1 }
   end
 
   factory :nominal_metric_observation do
     association :virtual_machine_instance, factory: :virtual_machine_instance
     # NOTE: metric might be associated with other benchmark_definition than created via virtual_machine_instance dependent factories
     association :metric_definition, factory: :nominal_metric_definition #, benchmark_definition: virtual_machine_instance.benchmark_execution.benchmark_definition
-    value 'Intel(R) Xeon(R) CPU E5-2673 v4 @ 2.30GHz'
+    value { 'Intel(R) Xeon(R) CPU E5-2673 v4 @ 2.30GHz' }
   end
 
   factory :user do
-    email Rails.application.config.default_email
-    password Rails.application.config.default_password
+    email { Rails.application.config.default_email }
+    password { Rails.application.config.default_password }
   end
 end
