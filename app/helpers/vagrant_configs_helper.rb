5078d2fe50d17b044646d5d0adf3bc105019be8c
Joel Scheuner <joel.scheuner.dev@gmail.com>
2019-01-02 19:17:31 +0100

    Add links to Vagrantfile docs

--- app/helpers/vagrant_configs_helper.rb
+++ app/helpers/vagrant_configs_helper.rb
@@ -2,4 +2,8 @@ module VagrantConfigsHelper
   def base_file_error?(vagrant_config)
     vagrant_config.errors[:base_file].any?
   end
+
+  def provider_docs(name, link, icon = 'github')
+    link_to "vagrant-#{name}&nbsp;#{fa_icon(icon)}".html_safe, link, target: '_blank'
+  end
 end
