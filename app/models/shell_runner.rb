2a7b2226f953adfafd07cb991ff84c4fef1d385e
Joel Scheuner <joel.scheuner.dev@gmail.com>
2019-02-24 20:54:44 +0100

    Extract method
    Separate command creation from execution for better testability.

--- app/models/shell_runner.rb
+++ app/models/shell_runner.rb
@@ -9,13 +9,18 @@ module ShellRunner
   #   log: file path for logging stdout and stderr
   # @return [Boolean] `true` if successful (i.e,. exit 0), `false` otherwise
   def shell(cmd, opts)
+    full_cmd = shell_cmd(cmd, opts)
+    system(full_cmd)
+    $?.success?
+  end
+
+  def shell_cmd(cmd, opts)
     full_cmd = ''.dup
     full_cmd << "cd #{opts[:dir]} && " if opts[:dir]
     full_cmd << reset_env
     full_cmd << cmd
     full_cmd << shell_log(opts[:log]) if opts[:log]
-    system(full_cmd)
-    $?.success?
+    full_cmd
   end
 
   # Within the production environment, it could be the case the environment variables pollute
