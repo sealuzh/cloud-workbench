6e5db71679504a504ca1bfe0fa3a242ca2df1a52
Joel Scheuner <joel.scheuner.dev@gmail.com>
2019-02-24 21:02:09 +0100

    Add ShellRunner#shell_cmd specs

--- spec/models/shell_runner_spec.rb
+++ spec/models/shell_runner_spec.rb
@@ -10,4 +10,15 @@ describe ShellRunner do
       expect(runner_instance.shell('false', {})).to be false
     end
   end
+  describe '#shell_cmd' do
+    it 'should compose a basic command and reset RUBYLIB' do
+      expect(runner_instance.shell_cmd('mycmd', {})).to eq "RUBYLIB='' mycmd"
+    end
+    it 'should change into the provided directory' do
+      expect(runner_instance.shell_cmd('pwd', dir: '/tmp')).to eq "cd /tmp && RUBYLIB='' pwd"
+    end
+    it 'should setup log redirect' do
+      expect(runner_instance.shell_cmd('echo', log: '/tmp/log.txt')).to eq "RUBYLIB='' echo >>/tmp/log.txt 2>&1"
+    end
+  end
 end
