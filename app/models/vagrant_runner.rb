dfe5261578dd10ebd05564f51d9ef3a58486213c
Joel Scheuner <joel.scheuner.dev@gmail.com>
2019-02-24 21:30:08 +0100

    Add parameter
    Makes ssh_command method more generic and its body more readable

--- app/models/vagrant_runner.rb
+++ app/models/vagrant_runner.rb
@@ -16,10 +16,10 @@ class VagrantRunner
     ssh_command(Rails.application.config.vm_start_postprocessing)
   end
 
-  def ssh_command(executable, dir = Rails.application.config.vm_benchmark_dir)
+  def ssh_command(executable, dir = Rails.application.config.vm_benchmark_dir, log = Rails.application.config.vm_error_log_file)
     # NOTE: We cannot use && between cd and nohup because this doesn't work together with non-blocking commands
     shell_cmd = "vagrant ssh -- \"cd '#{dir}';
-    nohup './#{executable}' >/dev/null 2>>'#{Rails.application.config.vm_error_log_file}' </dev/null &\""
+    nohup './#{executable}' >/dev/null 2>>'#{log}' </dev/null &\""
     shell(shell_cmd, dir: @vagrant_dir)
   end
 end
