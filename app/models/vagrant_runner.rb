a2591c55181f30925061bbdbb87e081f86cf13d7
Joel Scheuner <joel.scheuner.dev@gmail.com>
2019-02-25 13:50:28 +0100

    Add ssh method comments

--- app/models/vagrant_runner.rb
+++ app/models/vagrant_runner.rb
@@ -16,11 +16,13 @@ class VagrantRunner
     ssh_exec(Rails.application.config.vm_start_postprocessing)
   end
 
+  # Execute the given `executable` via SSH remotely
   def ssh_exec(executable, dir = Rails.application.config.vm_benchmark_dir, log = Rails.application.config.vm_error_log_file)
     shell_cmd = ssh_shell_cmd(executable, dir, log)
     shell(shell_cmd, dir: @vagrant_dir)
   end
 
+  # Compose SSH shell command for triggering an `executable` remotely
   def ssh_shell_cmd(executable, dir, log)
     # NOTE: We cannot use && between cd and nohup because this doesn't work together with non-blocking commands
     "vagrant ssh -- \"cd '#{dir}';
