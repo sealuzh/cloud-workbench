23d033a54b72d081f728a7338ca165d5ea740d44
Joel Scheuner <joel.scheuner.dev@gmail.com>
2019-02-25 17:08:06 +0100

    Add listing benchmark executions spec

--- spec/features/benchmark_executions_spec.rb
+++ spec/features/benchmark_executions_spec.rb
@@ -6,6 +6,21 @@ feature 'Benchmark execution' do
   given(:user) { create(:user) }
   before { sign_in(user) }
 
+  feature 'Listing' do
+    given(:e1) { create(:benchmark_execution) }
+    given(:e2) { create(:benchmark_execution) }
+    background do
+      e1.save!
+      e2.save!
+      visit benchmark_executions_path
+    end
+
+    scenario 'Should show all benchmark executions' do
+      # -1 because of the header row
+      expect(find('.table').all('tr').size - 1).to eq 2
+    end
+  end
+
   feature 'Showing a benchmark execution' do
     given(:benchmark_execution) { create(:benchmark_execution) }
     background { visit benchmark_execution_path(benchmark_execution) }
